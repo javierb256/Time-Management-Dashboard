9e29f92ada2940b5c2adfb148851521908d620d9
javierb256 <44247277+javierb256@users.noreply.github.com>
2021-10-05 00:11:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    // TODO 1: Create small cards
+    // TODO 2: Create larger card for user info
+    // TODO 3: show all 6 small cards with various info
+    // TODO 4: create a layout component for proper card placement
+    //BONUS: add card animations?
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
