45e7e687786edffe2f17e19d6a5330cd0cc60df5
javierb256 <44247277+javierb256@users.noreply.github.com>
2021-11-05 13:42:17 -0700

    changed error in layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,12 +2,11 @@
 import Hours from "../Information/Hours";
 import User from "../Information/User";
 import classes from "../layout/Layout.module.css";
-import numbers from "../data.json";
+import data from "../data.json";
 import { useState } from "react";
 
 
 function Layout() {
-  const [data, setData] = useState(numbers);
   const [time, setTime] = useState("daily");
   return (
     <div className={classes.mainContainer}>
